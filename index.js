8f93648d39fc86a2ede2568454d541454897e883
xiaowu <3103506013@qq.com>
2024-06-19 14:20:35 +0800

    取消currentCurve中间变量；对曲线移动的处理收归到曲线定义中

--- index.js
+++ index.js
@@ -94,7 +94,6 @@ function calculateIntersection(line1, line2) {
   return {x, y}
 }
 
-let currentCurve = ''; // 保留，在选中线段时使用
 const SELECT_RANGE = 25;
 
 let demandCurve = {
@@ -103,15 +102,28 @@ let demandCurve = {
   end: { x: 600, y: 100 },
   equation: lineFormula({x: 200, y: 400}, {x: 600, y: 100}),
   width: 600 - 200,
-  create(ctx, currentCurve) {
+  isSelect: false,
+  create(ctx, offset = {start: 0, end: 0}) {
     ctx.save();
     ctx.beginPath();
+    ctx.lineWidth = "2";
+    if (this.isSelect) {
+      ctx.strokeStyle = '#ffc107';
+      this.start.x += offset.start;
+      this.end.x += offset.end;
+      if (this.start.x <= 100) {
+        this.start.x = 100;
+        this.end.x = 100 + this.width;
+      }
+    
+      if (this.end.x >= 700) {
+        this.end.x = 700;
+        this.start.x = 700 - this.width;
+      }
+      this.equation = lineFormula(this.start, this.end);
+    }
     ctx.moveTo(logicXToRealX(this.start.x), logicYToRealY(this.start.y));
     ctx.lineTo(logicXToRealX(this.end.x), logicYToRealY(this.end.y));
-    ctx.lineWidth = "2";
-    if (currentCurve.name == this.name) {
-      ctx.strokeStyle = '#ffc107';
-    }
     ctx.stroke();
     ctx.closePath();
     ctx.restore();
@@ -124,15 +136,29 @@ let supplyCurve = {
   end: { x: 600, y: 400 },
   equation: lineFormula({ x: 200, y: 100 }, { x: 600, y: 400 }),
   width: 600 - 200,
-  create(ctx, currentCurve) {
+  isSelect: false,
+  create(ctx, offset = {start: 0, end: 0}) {
     ctx.save();
     ctx.beginPath();
+    ctx.lineWidth = "2";
+    if (this.isSelect) {
+      ctx.strokeStyle = '#ffc107';
+      this.start.x += offset.start;
+      this.end.x += offset.end;
+      if (this.start.x <= 100) {
+        this.start.x = 100;
+        this.end.x = 100 + this.width;
+      }
+    
+      if (this.end.x >= 700) {
+        this.end.x = 700;
+        this.start.x = 700 - this.width;
+      }
+      this.equation = lineFormula(this.start, this.end);
+    }
+    
     ctx.moveTo(logicXToRealX(this.start.x), logicYToRealY(this.start.y));
     ctx.lineTo(logicXToRealX(this.end.x), logicYToRealY(this.end.y));
-    ctx.lineWidth = "2";
-    if (currentCurve.name == this.name) {
-      ctx.strokeStyle = '#ffc107';
-    }
     ctx.stroke();
     ctx.closePath();
     ctx.restore();
@@ -149,8 +175,8 @@ class NormalSupplyDemand {
   init() {
     let ctx = this.ctx;
     createCoordinateSystem(ctx);
-    demandCurve.create(ctx, currentCurve);
-    supplyCurve.create(ctx, currentCurve);
+    demandCurve.create(ctx);
+    supplyCurve.create(ctx);
     
     this.canvas.addEventListener('mousedown', e => this.handleMousedown(e));
     this.canvas.addEventListener('mousemove', e => this.handleMousemove(e));
@@ -167,64 +193,58 @@ class NormalSupplyDemand {
 
     // 选中需求曲线
     if (demandCurveDistance < SELECT_RANGE) {
-      currentCurve = demandCurve;
+      demandCurve.isSelect = true;
       this.repaintCoordinateSystem();
       ctx.save();
       ctx.strokeStyle = '#ffc107';
-      demandCurve.create(ctx, currentCurve);
+      demandCurve.create(ctx);
       ctx.restore();
-      supplyCurve.create(ctx, currentCurve);
+      supplyCurve.create(ctx);
       this.drawIntersection(calculateIntersection(demandCurve.equation, supplyCurve.equation));
     }
     
     // 选中供给曲线
     if (supplyCurveDistance < SELECT_RANGE && demandCurveDistance >= SELECT_RANGE) {
-      currentCurve = supplyCurve;
+      supplyCurve.isSelect = true;
       this.repaintCoordinateSystem();
-      demandCurve.create(ctx, currentCurve);
+      demandCurve.create(ctx);
       ctx.save();
       ctx.strokeStyle = '#ffc107';
-      supplyCurve.create(ctx, currentCurve);
+      supplyCurve.create(ctx);
       ctx.restore();
       this.drawIntersection(calculateIntersection(demandCurve.equation, supplyCurve.equation));
     }
 
-    if (currentCurve == '') return
-    this.isMousedown = true;
-    this.lastX = e.clientX;
+    if (supplyCurve.isSelect || demandCurve.isSelect) {
+      this.isMousedown = true;
+      this.lastX = e.clientX;
+    }
+
   }
   handleMousemove(e) {
     if (!this.isMousedown) return
     let ctx = this.ctx;
 
     this.repaintCoordinateSystem();
-    currentCurve.start.x += realXToLogicX(e.clientX - this.lastX);
-    currentCurve.end.x += realXToLogicX(e.clientX - this.lastX);
+    let offsetX = realXToLogicX(e.clientX - this.lastX);
+    
+    let offset = {
+      start: offsetX,
+      end: offsetX
+    };
+    demandCurve.create(ctx, offset);
+    supplyCurve.create(ctx, offset);
     this.lastX = e.clientX;
-  
-    if (currentCurve.start.x <= 100) {
-      currentCurve.start.x = 100;
-      currentCurve.end.x = 100 + currentCurve.width;
-    }
-  
-    if (currentCurve.end.x >= 700) {
-      currentCurve.end.x = 700;
-      currentCurve.start.x = 700 - currentCurve.width;
-    }
-  
-    currentCurve.equation = lineFormula(currentCurve.start, currentCurve.end);
-  
-    demandCurve.create(ctx, currentCurve);
-    supplyCurve.create(ctx, currentCurve);
     this.drawIntersection(calculateIntersection(demandCurve.equation, supplyCurve.equation));
   }
   handleMouseup(e) {
     if (!this.isMousedown) return 
     let ctx = this.ctx;
     this.isMousedown = false;
-    currentCurve = ''; 
-    demandCurve.create(ctx, currentCurve);
-    supplyCurve.create(ctx, currentCurve);
+    demandCurve.isSelect = false;
+    supplyCurve.isSelect = false;
+    demandCurve.create(ctx);
+    supplyCurve.create(ctx);
   }
   drawIntersection(point) {
     const x = logicXToRealX(point.x);
